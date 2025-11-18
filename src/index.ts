f5703cae691c412bca5701eb79f944ac373e896e
vinngo <ngovincent9593@gmail.com>
2025-11-17 17:56:16 -0800

    add: chat handler

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
+import chatHandler from "./handlers/chat";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .use(chatHandler)
+  .listen(8000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
